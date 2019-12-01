9fc91b5c7158e542a4e61beedd97cdce79dbb547
Sukasa <nick.m@shaw.ca>
2019-12-01 13:12:04 -0800

    Day 1 completed

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using System.IO;
+using System;
+using System.Linq;
 
 namespace Day1
 {
     class Program
     {
+        static int FuelNeed(int Fuel)
+        {
+            return Fuel / 3 - 2;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var FuelRequirements = File.ReadLines("input.txt").Select(x => FuelNeed(int.Parse(x)));
+            var SumFuel = FuelRequirements.Sum();
+            Console.Write("Naive fuel req: ");
+            Console.WriteLine(SumFuel);
+            Console.WriteLine("");
+            while (FuelRequirements.Any())
+            {
+                FuelRequirements = FuelRequirements.Select(x => FuelNeed(x)).Where(x => x > 0);
+                SumFuel += FuelRequirements.Sum();
+            }
+
+            Console.Write("Cascading fuel req: ");
+            Console.WriteLine(SumFuel);
+            Console.WriteLine("");
+
+            Console.ReadLine();
         }
     }
 }
