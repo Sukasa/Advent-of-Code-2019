f56da53a7991f38922fcee75717a7582c60e523a
Sukasa <nick.m@shaw.ca>
2019-12-03 22:02:18 -0800

    Optimize Day 4, for the fun of it

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Day4
 {
@@ -6,23 +7,34 @@ namespace Day4
     {
         static void Main(string[] args)
         {
-            int matches = 0;
-            for (int check = 206938; check <= 679128; check++)
+            Stopwatch Sw = new Stopwatch();
+            Sw.Start();
+            for (int test = 0; test < 100; test++)
             {
-                string Adj = " " + check.ToString() + " ";
-                if (NoDescending(Adj) && HasDouble(Adj))
-                    matches++;
+                int matches = 0;
+                int add;
+                for (int check = 206938; check <= 679128; check++)
+                {
+                    add = 0;
+                    string Adj = " " + check.ToString() + " ";
+                    for (int i = 1; i < Adj.Length - 2; i++)
+                    {
+                        if (Adj[i] > Adj[i + 1])
+                            goto Skip;
+
+                        if (add == 1 || ((Adj[i] == Adj[i + 1]) && (Adj[i] != Adj[i + 2]) && (Adj[i] != Adj[i - 1])))
+                            add = 1;
+                    }
+
+                    matches += add;
+
+                Skip:;
+                }
+
+                Console.WriteLine(matches);
             }
-
-            Console.WriteLine(matches);
-        }
-
-        static bool NoDescending(string Input)
-        {
-            for (int i = 1; i < Input.Length - 2; i++)
-                if (Input[i] > Input[i + 1])
-                    return false;
-            return true;
+            Sw.Stop();
+            Console.WriteLine("Avg {0}ms to check {1} possible combinations", (double)Sw.ElapsedMilliseconds / 100, 679128 - 206938 + 1);
         }
 
         static bool HasDouble(string Input)
